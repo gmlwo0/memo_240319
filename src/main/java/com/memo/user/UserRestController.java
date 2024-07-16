28f3357fb99878cd2b404890c0df064c71d903bf
DESKTOP-QSFTC3I\Administrator <taeci123@naver.com>
2024-07-17 04:35:32 +0900

    2024-07-16 집

--- src/main/java/com/memo/user/UserRestController.java
+++ src/main/java/com/memo/user/UserRestController.java
@@ -28,27 +28,26 @@ public class UserRestController {
 	 * @param loginId
 	 * @return
 	 */
-	
 	@RequestMapping("/is-duplicated-id")
-	public Map<String,Object> isDuplicatedId(
-			@RequestParam("loginId") String loginId){
+	public Map<String, Object> isDuplicatedId(
+			@RequestParam("loginId") String loginId) {
 		
 		// db 조회
 		UserEntity user = userBO.getUserEntityByLoginId(loginId);
 		
 		// 응답값
-		Map<String,Object> result = new HashMap<>();
+		Map<String, Object> result = new HashMap<>();
 		result.put("code", 200);
 		if (user != null) {
-			result.put("is-duplicated-id", true);			
+			result.put("is_duplicated_id", true);
 		} else {
-			result.put("is-duplicated-id", false);			
+			result.put("is_duplicated_id", false);
 		}
 		return result;
 	}
 	
 	/**
-	 * 회원가입
+	 * 회원가입 API
 	 * @param loginId
 	 * @param password
 	 * @param name
@@ -56,31 +55,30 @@ public class UserRestController {
 	 * @return
 	 */
 	@PostMapping("/sign-up")
-	public Map<String,Object> signUp(
+	public Map<String, Object> signUp(
 			@RequestParam("loginId") String loginId,
 			@RequestParam("password") String password,
 			@RequestParam("name") String name,
-			@RequestParam("email") String email){
+			@RequestParam("email") String email) {
 		
-		// password md5 알고리즘 => hashing
-		// aaaa => 
+		// password md5 알고리즘 => hashing   
+		// aaaa => 74b8733745420d4d33f80c4663dc5e5
+		// aaaa => 74b8733745420d4d33f80c4663dc5e5
 		String hashedPassword = EncryptUtils.md5(password);
 		
 		// db insert
-		UserEntity user =  userBO.addUser(loginId,hashedPassword,name,email);
-
+		UserEntity user = userBO.addUser(loginId, hashedPassword, name, email);
 		
 		// 응답값
-		Map<String,Object> result = new HashMap<>();
+		Map<String, Object> result = new HashMap<>();
 		if (user != null) {
-			result.put("code",200);
-			result.put("result","성공");			
-		}else {
-			result.put("code",500);
-			result.put("error_message","회원가입에 실패했습니다");					
+			result.put("code", 200);
+			result.put("result", "성공");
+		} else {
+			result.put("code", 500);
+			result.put("error_message", "회원가입에 실패했습니다.");
 		}
 		return result;
-		
 	}
 	
 	/**
@@ -91,10 +89,10 @@ public class UserRestController {
 	 * @return
 	 */
 	@PostMapping("/sign-in")
-	public Map<String,Object> signIn(
+	public Map<String, Object> signIn(
 			@RequestParam("loginId") String loginId,
 			@RequestParam("password") String password,
-			HttpServletRequest request){
+			HttpServletRequest request) {
 		
 		// password 해싱
 		String hashedPassword = EncryptUtils.md5(password);
@@ -102,17 +100,17 @@ public class UserRestController {
 		// DB 조회 - loginId, 해싱된 비밀번호 => UserEntity
 		UserEntity user = userBO.getUserEntityByLoginIdPassword(loginId, hashedPassword);
 		
-		// 로그인 처리
-		Map<String,Object> result = new HashMap<>();
+		// 로그인 처리 및 응답값
+		Map<String, Object> result = new HashMap<>();
 		if (user != null) { // 성공
 			// 세션에 사용자 정보를 담는다.(사용자 각각 마다)
 			HttpSession session = request.getSession();
-			session.setAttribute("userId",user.getId());
+			session.setAttribute("userId", user.getId());
 			session.setAttribute("userLoginId", user.getLoginId());
 			session.setAttribute("userName", user.getName());
 			
 			result.put("code", 200);
-			result.put("result", "성공");			
+			result.put("result", "성공");
 		} else { // 실패
 			result.put("code", 403);
 			result.put("error_message", "존재하지 않는 사용자입니다.");
@@ -120,6 +118,4 @@ public class UserRestController {
 		
 		return result;
 	}
-	
-	
 }
--- src/main/java/com/memo/user/bo/UserBO.java
+++ src/main/java/com/memo/user/bo/UserBO.java
@@ -18,12 +18,11 @@ public class UserBO {
 		return userRepository.findByLoginId(loginId);
 	}
 	
-	//  input: loginId,password
+	// input: loginId, password
 	// output: UserEntity or null
 	public UserEntity getUserEntityByLoginIdPassword(String loginId, String password) {
-		return userRepository.findByLoginIdAndPassword(loginId,password);
+		return userRepository.findByLoginIdAndPassword(loginId, password);
 	}
-	
 	
 	// input: 4파라미터
 	// output: UserEntity
